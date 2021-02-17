7bfa961b512eadd52e7af6c9f2396c7f6c9bb3ce
Henrique Hadermeck <henriquezanferrari@gmail.com>
2021-02-17 10:26:54 -0300

    Exercício Árvore TRIE - resolução

--- exercicios/arvore_tri/trie.cpp
+++ exercicios/arvore_tri/trie.cpp
@@ -1,64 +1,196 @@
 #include "trie.h"
 
+#include <stdio.h>
+
 // DICA: Estude o arquivo trie.h antes de iniciar a sua implementacao!
 
 no *criarNo(void)
 {
-    // IMPLEMENTAR !!!
-    // Dica: Aloca um no, atribua o tipo como 'I' e todos os filhos como NULL.
-    return NULL;
+    no *node = (no *)malloc(sizeof(no));
+
+    if (node)
+    {
+        int i;
+        node->tipo = 'I';
+        for (i = 0; i < TAMANHO_ALFABETO; i++)
+            node->filho[i] = NULL;
+    }
+
+    return node;
 }
 
 void apagarArvore(no *raiz)
 {
-    // IMPLEMENTAR !!!
-    // Dica 1: Desaloque todos os nos filhos e depois desaloca o no atual.
-    // Dica 2: Usar recursao facilita!
+    int i;
+    for (i = 0; i < TAMANHO_ALFABETO; i++)
+    {
+        if (raiz->filho[i] != NULL)
+        {
+            apagarArvore(raiz->filho[i]);
+            raiz->filho[i] == NULL;
+        }
+    }
+
+    free(raiz);
 }
 
 void adicionarPalavra(char *palavra, no *raiz)
 {
-    // IMPLEMENTAR !!!
-    // Dica 1: Use um laco iterativo de 0 ate strlen(palavra)
-    // Dica 2: Criar o no com a funcao criarNo (caso nao existir)
-    // Dica 3: Utilize a macro CHAR_TO_INDEX (ver trie.h) para converter o caractere para o indice filho
-    // Dica 4: Nao esqueca de mudar o tipo para 'P' no ultimo noh que representa o ultimo caractere
+    int nivel = 0, indice;
+    no *aux = raiz;
+
+    for (nivel = 0; nivel < strlen(palavra); nivel++)
+    {
+        indice = CHAR_TO_INDEX(palavra[nivel]);
+        if (aux->filho[indice] == NULL)
+            aux->filho[indice] = criarNo();
+
+        aux = aux->filho[indice];
+    }
+
+    aux->tipo = 'P';
 }
 
 int buscaPalavra(char *palavra, no *raiz)
 {
-    // IMPLEMENTAR !!!
-    // Dica 1: Funcao similar ao adicionarPalavra
-    // Dica 2: Se o ultimo noh que representa o ultimo caractere for do tipo 'I', a palavra nao existe
-    // IMPORTANTE:
-    //   retorne 0 se a palavra nao exite
-    //   retorne 1 se a palavra existir
-    return 0;
+    int i = 0, indice;
+
+    no *aux = raiz;
+
+    for (i = 0; i < strlen(palavra); i++)
+    {
+        indice = CHAR_TO_INDEX(palavra[i]);
+        if (aux->filho[indice] != NULL)
+            aux = aux->filho[indice];
+        else
+            break;
+    }
+
+    if (palavra[i] == '\0' && aux->tipo == 'P')
+        return 1;
+    else
+        return 0;
 }
 
 int numeroDeNos(no *r)
 {
-    // IMPLEMENTAR !!!
-    // Dica: A funcao pode ser muito simples se utilizar recursao
-    return 0;
+    int i, numero = 0;
+
+    for (i = 0; i < TAMANHO_ALFABETO; i++)
+    {
+        if (r->filho[i] != NULL)
+        {
+            numero += numeroDeNos(r->filho[i]);
+        }
+    }
+
+    return numero + 1;
 }
 
 int numeroDePalavras(no *r)
 {
-    // IMPLEMENTAR !!!
-    // Dica: Similar a funcao numeroDeNos, mas contabilize apenas os tipos 'P', que representa as palavras
-    return 0;
+    int i, numero = 0;
+
+    if (r->tipo == 'P')
+    {
+        numero++;
+    }
+    for (i = 0; i < TAMANHO_ALFABETO; i++)
+    {
+        if (r->filho[i] != NULL)
+        {
+            numero += numeroDePalavras(r->filho[i]);
+        }
+    }
+
+    return numero;
 }
 
 int altura(no *r)
 {
-    // IMPLEMENTAR !!!
-    // Dica: A funcao pode ser muito simples se utilizar recursao
-    return 0;
+    int i, ref = 0;
+
+    if (!r)
+        return 0;
+
+    for (i = 0; i < TAMANHO_ALFABETO; i++)
+    {
+        int numero = altura(r->filho[i]);
+        if (numero > ref)
+        {
+            ref = numero;
+            return ref + 1;
+        }
+    }
+
+    return 1;
 }
 
 void removerPalavra(char *palavra, no *raiz)
 {
-    // IMPLEMENTAR !!!
-    // Nota: Esta funcao eh a mais elaborada. Recomendo criar funcoes auxiliares e utilizar recursao
+    int i = 0, indice;
+
+    no *aux = raiz;
+
+    for (i = 0; i < strlen(palavra); i++)
+    {
+        indice = CHAR_TO_INDEX(palavra[i]);
+        if (aux->filho[indice] != NULL)
+            aux = aux->filho[indice];
+        else
+            break;
+    }
+
+    if (palavra[i] == '\0' && aux->tipo == 'P')
+        aux->tipo = 'I';
+}
+
+void imprimeRaiz(no *raiz)
+{
+    int i;
+
+    printf("(%c:", raiz->tipo);
+    for (i = 0; i < TAMANHO_ALFABETO; i++)
+    {
+        if (raiz->filho[i] != NULL)
+        {
+            printf("%c", (int)'a' + (int)i);
+            imprimeRaiz(raiz->filho[i]);
+        }
+    }
+
+    printf(")");
+}
+
+int main()
+{
+    no *raiz = criarNo();
+
+    adicionarPalavra("pal", raiz);
+    adicionarPalavra("pa", raiz);
+    adicionarPalavra("pato", raiz);
+    adicionarPalavra("pate", raiz);
+    adicionarPalavra("pata", raiz);
+    // adicionarPalavra("zebra", raiz);
+    // adicionarPalavra("abacaxi", raiz);
+    // adicionarPalavra("vermelho", raiz);
+    // adicionarPalavra("martelo", raiz);
+
+    imprimeRaiz(raiz);
+
+    removerPalavra("pato", raiz);
+
+    imprimeRaiz(raiz);
+    // int numeroPalavras = numeroDePalavras(raiz);
+    // printf("\nNumero de palavras: %d", numeroPalavras);
+    // int numeroNos = numeroDeNos(raiz);
+    // printf("\nNumero de nos: %d", numeroNos);
+
+    // int temPalavra = buscaPalavra("arvore", raiz);
+
+    // printf("%d", temPalavra);
+
+    apagarArvore(raiz);
+
+    return 0;
 }
